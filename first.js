d9d75a25fa9bc07df70878895f317e6720b9ffd7
Pushp Raj Bharti <pushprajbharti25@gmail.com>
2026-01-27 18:11:39 +0530

    My Second commit

---
+++ first.js
@@ -0,0 +1,27 @@
+const name = "Pushp Raj Bharti";
+const age =20;
+const balance = 5000;
+
+console.log("Hello");
+function updateBalance(){
+    balance+=20;
+}
+
+updateBalance();
+
+console.log(balance);
+
+console.log("Money honey");
+console.log("Bug final commit");
+console.log("Mein hu hero tera");
+
+function showage(val){
+    console.log(age+val);
+}
+
+showage(10);
+function showName(){
+    console.log(name);
+}
+
+showName();
