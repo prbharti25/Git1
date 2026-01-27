311bb58bb50ee6e1381da13b7c8f524cef6e5d78
Pushp Raj Bharti <pushprajbharti25@gmail.com>
2026-01-27 18:44:14 +0530

    4th commit

--- first.js
+++ first.js
@@ -14,3 +14,10 @@ console.log(balance);
 console.log("Money is very important, it can give you immense happiness!");
 console.log("Bug final commit");
 console.log("Mein hu hero tera");
+
+function showage(val){
+    console.log(age+val);
+}
+
+showage(10);
+
